d9e6ec02a410945000060e4b47fb0413c7f6991f
Valeriia-Rez <Valeria09122016@gmail.com>
2019-11-11 11:16:50 +0200

    change app.js, updating the budget:controller

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ var UIController = (function(){
             return{
                 type: document.querySelector(DOMstrings.inputType).value,//Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -143,12 +143,23 @@ var controller = (function(budgetCtrl,UICtrl){
     });
 };
 
+    var updateBudget = function(){
+
+        //1.Calculate the budget
+
+
+        //2.Return the budget
+
+        //3.Display the budget on the UI
+    }
+
     var ctrlAddItem = function(){
         var input, newItem;
         //1.Get the field input data
         input = UICtrl.getInput();
         
-        //2.Add the item to the budget controller
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+            //2.Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         //3.Add the item to the UI
@@ -156,8 +167,10 @@ var controller = (function(budgetCtrl,UICtrl){
 
         //4.Clear the field
         UICtrl.clearFields();
-        //5.Display the budget on the UI
-        
+
+        //5.Calculate and update budget
+        updateBudget();
+        }
     };
 
     return {
