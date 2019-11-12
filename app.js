3a05bf515f1aa8e9d784f6b7190d2012061fedee
Valeriia-Rez <Valeria09122016@gmail.com>
2019-11-12 14:39:39 +0200

    change app.js,setting up the delete event listener using event delegation

--- app.js
+++ app.js
@@ -106,7 +106,8 @@ var UIController = (function(){
         budgetLabel: ".budget__value",
         incomeLabel: ".budget__income--value",
         expensesLabel:".budget__expenses--value",
-        percentageLabel:".budget__expenses--percentage"
+        percentageLabel:".budget__expenses--percentage",
+        container: ".container"
     };
     return {
         getInput: function(){
@@ -188,6 +189,8 @@ var controller = (function(budgetCtrl,UICtrl){
             ctrlAddItem();
         }
     });
+
+    document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
 };
 
     var updateBudget = function(){
@@ -219,6 +222,30 @@ var controller = (function(budgetCtrl,UICtrl){
         //5.Calculate and update budget
         updateBudget();
         }
+    };
+
+    var ctrlDeleteItem = function(event){
+
+        var itemId, splitId, type, ID;
+
+        itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+        if(itemId){
+
+            //inc-1
+            splitId = itemId.split("-");
+            type = splitId[0];
+            ID = splitId[1];
+
+            //1.delete the item from the data structure
+
+            //2.delete the item from the UI
+
+            //3.update and show the new budget
+
+            
+        }
+
     };
 
     return {
